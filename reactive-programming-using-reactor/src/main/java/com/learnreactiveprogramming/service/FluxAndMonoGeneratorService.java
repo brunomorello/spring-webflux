c921501bb05f33f94d74f9933c25546d744afaf4
Bruno Moreno <brunomorello7@gmail.com>
2023-10-01 16:15:01 +0100

    Initialize Repository Created Flux and Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,25 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
 public class FluxAndMonoGeneratorService {
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(Arrays.asList("Luna", "Izzy", "Keira"));
+    }
+
+    public Mono<String> namesMono() {
+        return Mono.just("Mel");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> System.out.println(String.format("[FLUX] Cat name %s", name)));
+
+        fluxAndMonoGeneratorService.namesMono()
+                .subscribe(name -> System.out.println(String.format("[MONO] Cat name %s", name)));
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,19 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    private FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    @Test
+    void when_flux_of_strings_validate_its_values_and_completion() {
+
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+        StepVerifier.create(namesFlux)
+                .expectNext("Luna", "Izzy", "Keira")
+                .verifyComplete();
+    }
 }
